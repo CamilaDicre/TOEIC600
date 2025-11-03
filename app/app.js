bb71159af46cbe94424e71997c43f754744a813d
CamilaDicre <204351908+CamilaDicre@users.noreply.github.com>
2025-11-03 15:38:44 -0500

    Add javascript template

---
+++ app/app.js
@@ -0,0 +1,3 @@
+import { units, words } from './data.js'
+
+console.log(units[1]);
---
+++ app/data.js
@@ -0,0 +1,18 @@
+const units = {
+    '1' : 'Banking',
+    '2' : 'Shopping',
+    '3' : 'Shipping',
+    '4' : 'Accounting',
+}
+
+const words = {
+    '1' : {
+        'bill' : 'something to pay for',
+        'account' : 'where you save your money',
+        'save' : 'to not spent money'
+    },
+}
+
+export { units, words }
+
+// 'units' holds the description of each unit, while 'words' holds the vocabulary for each unit.
