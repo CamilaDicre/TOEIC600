4ed9eb73da860cfa72b98f4d3014bc3c29b52cc7
CamilaDicre <204351908+CamilaDicre@users.noreply.github.com>
2025-11-04 14:55:12 -0500

    Add unit outline functionality and flashcards functionality base

--- app/app.js
+++ app/app.js
@@ -2,25 +2,66 @@ import { units, words } from './data.js'
 
 (function() {
 
+// VARIABLES
+
 let currentUnit = 0;
 
 const secOverview = document.getElementById('overview');
 const secOutline = document.getElementById('outline');
+const secFlashcards = document.getElementById('flashcards');
+
+let cards = [];
+
+// FUNCTIONS
 
 function showOverview() {
-    secOverview.style.display = 'block'
-    secOutline.style.display = 'none'
+    secOverview.style.display = 'block';
+    secOutline.style.display = 'none';
+    secFlashcards.style.display = 'none';
 }
 
 function showOutline() {
     secOverview.style.display = 'none'
     secOutline.style.display = 'block'
+
+    document.getElementById('unitNameOutline').textContent = units[currentUnit];
+    document.getElementById('words').textContent = Object.keys(words[currentUnit]).length;
+
+    document.getElementById('unitSummary').replaceChildren()
+
+    for (const word in words[currentUnit]) {
+        let item = document.createElement('li');
+        let desc = document.createElement('span');
+        desc.textContent = words[currentUnit][word];
+        item.textContent = word;
+        item.appendChild(desc)
+        document.getElementById('unitSummary').appendChild(item);
+    };
 };
+
+function showFlashcards() {
+    secFlashcards.style.display = 'block';
+    secOutline.style.display = 'none';
+    secOverview.style.display = 'none';
+
+    document.getElementById('unitNameFlashcards').textContent = units[currentUnit];
+
+    // PREPARE FLASHCARDS
+
+};
+
+// EVENT LISTENERS
 
 document.getElementById('goToOverview').addEventListener('click', () => {
     showOverview();
     return;
 });
+
+document.getElementById('startFlashcards').addEventListener('click', () => {
+    showFlashcards();
+});
+
+// RENDER FUNCTION
 
 function render() {
     for (const unit in units) {
@@ -49,5 +90,6 @@ function render() {
 };
 
 
-render()
+render();
+showOverview();
 })();
