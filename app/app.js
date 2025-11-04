6110586553a3eae189cd58341e516c723d5d8c92
CamilaDicre <204351908+CamilaDicre@users.noreply.github.com>
2025-11-04 14:25:58 -0500

    Add unit Learn button and hide/show functionality to the units

--- app/app.js
+++ app/app.js
@@ -2,6 +2,26 @@ import { units, words } from './data.js'
 
 (function() {
 
+let currentUnit = 0;
+
+const secOverview = document.getElementById('overview');
+const secOutline = document.getElementById('outline');
+
+function showOverview() {
+    secOverview.style.display = 'block'
+    secOutline.style.display = 'none'
+}
+
+function showOutline() {
+    secOverview.style.display = 'none'
+    secOutline.style.display = 'block'
+};
+
+document.getElementById('goToOverview').addEventListener('click', () => {
+    showOverview();
+    return;
+});
+
 function render() {
     for (const unit in units) {
         let section = document.createElement('section');
@@ -10,6 +30,13 @@ function render() {
         heading.textContent = units[unit];
         let button = document.createElement('button');
         button.textContent = 'Learn'
+
+        button.addEventListener('click', () => {
+            currentUnit = section.getAttribute('unit')
+            console.log(currentUnit);
+            showOutline()
+        });
+
         let span = document.createElement('span');
         span.textContent = 'Words: ' + Object.keys(words[unit]).length
 
@@ -17,9 +44,9 @@ function render() {
         section.appendChild(span)
         section.appendChild(button);
 
-        document.querySelector('main').appendChild(section);
+        document.getElementById('overview').appendChild(section);
     };
-}
+};
 
 
 render()
