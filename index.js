57e76a438d975ffb75388a02176491dab2434400
CamilaDicre <204351908+CamilaDicre@users.noreply.github.com>
2025-11-08 16:20:04 -0500

    Change word carousel text changing functionality to an EventListener

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const CAROUSEL = document.getElementById('carousel');
 const WORD_BANK = ['Commensurate', 'Tax', 'Account', 'Balance', 'Overdraft', 'Book', 'Return'];
 let currentWord = 0;
 
-function startTimer() {
-    let timer = setInterval(() => {
+CAROUSEL.textContent = WORD_BANK[currentWord];
+CAROUSEL.style.animationName = 'fadeOut';
+
+CAROUSEL.addEventListener('animationend', (event) => {
+    if (CAROUSEL.style.animationName == 'fadeOut') {
         currentWord += 1;
         if (currentWord >= WORD_BANK.length) {
             currentWord = 0;
         }
         CAROUSEL.textContent = WORD_BANK[currentWord];
-    }, 3000)
-}
-
-CAROUSEL.textContent = WORD_BANK[currentWord];
-startTimer();
+        CAROUSEL.style.animationName = 'fadeIn';
+    } else {
+        CAROUSEL.style.animationName = 'fadeOut';
+    }
+});
